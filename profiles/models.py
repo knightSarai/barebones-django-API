3a6e6909e91f0f98fcd9f92788109261b5b9fe54
knightSarai <knight.sarai.dev@gmail.com>
2020-06-03 13:47:28 +0300

    FeedSerializer

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.conf import settings
+
 
 class UserProfileManager(BaseUserManager):
     """Manager for user profile"""
@@ -50,3 +52,14 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """class string represintation"""
         return self.email
+
+
+class ProfileFeed(models.Model):
+    """ Profile status Updates"""
+    user_profile = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add = True)
+    
+    def __str__(self):
+        """class string represintation"""
+        return self.status_text
